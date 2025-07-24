9f59e38b3893049eafbbc35fdc612cf1b8036962
STADTrainer <darshan.stadsolution@gmail.com>
2025-07-24 10:33:23 +0530

    first commit

---
+++ src/test/java/testngproject/Gitexample/Test.java
@@ -0,0 +1,11 @@
+package testngproject.Gitexample;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		// TODO Auto-generated method stub
+
+	}
+
+}
